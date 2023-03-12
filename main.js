a7ebc3144af3f8ac8ddac07fff313633364249e6
Danya Zavarin <lovec_omarov@mail.ru>
2023-03-12 22:14:01 +0300

    Updated main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+document.getElementById("main_form").addEventListener("submit", checkForm);
+
+function checkForm(event) {
+    event.preventDefault();
+    let form = document.querySelector("#main_form");
+    let name = form.name.value;
+    let pass = form.pass.value;
+    let repass = form.repass.value;
+    let state = form.state.value;
+    let fail = "";
+    if (name === "" || pass === "" || state === "") {
+        fail = "Заполните все поля";
+    } else if (name.length <= 1 || name.length > 50) {
+        fail = "Введите корректное имя";
+    } else if (pass !== repass) {
+        fail = "Пароли должны совпадать";
+    } else if (pass.split("&").length > 1) {
+        fail = "Некорректный пароль";
+    }
+    if (fail !== "") {
+        document.getElementById("error").innerHTML = fail;
+    } else {
+        alert("Все данные корректно заполнены");
+        window.location = "https://itproger.com";
+    }
+}
